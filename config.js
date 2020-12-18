068877e3f5084c300e2bbe4a86950515fd77d9cc
Doggycheems <75123663+Doggycheems@users.noreply.github.com>
2020-12-18 16:07:40 +0100

    Update config.js

--- config.js
+++ config.js
@@ -19,12 +19,12 @@ DATABASE_URL = process.env.DATABASE_URL === undefined ? './whatsasena.db' : proc
 DEBUG = process.env.DEBUG === undefined ? false : convertToBool(process.env.DEBUG);
 
 module.exports = {
-    VERSION: 'v0.9.0 Beta',
+    VERSION: 'v0.9.0 Beta Mod',
     SESSION: process.env.DOGGYCHEEMS_SESSION === undefined ? '' : process.env.DOGGYCHEEMS_SESSION,
     LANG: process.env.LANGUAGE === undefined ? 'TR' : process.env.LANGUAGE.toUpperCase(),
     HANDLERS: process.env.HANDLERS === undefined ? '^[.!;]' : process.env.HANDLERS,
     SEND_READ: process.env.SEND_READ === undefined ? false : convertToBool(process.env.SEND_READ),
-    BRANCH: 'master',
+    BRANCH: 'doggywhatsapp',
     HEROKU: {
         HEROKU: process.env.HEROKU === undefined ? false : convertToBool(process.env.HEROKU),
         API_KEY: process.env.HEROKU_API_KEY === undefined ? '' : process.env.HEROKU_API_KEY,
